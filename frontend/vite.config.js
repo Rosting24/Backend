57e550c00115933b2e5f038bb683146d8ad94d4f
Pranesz <praneshpranesh648@gmail.com>
2025-11-27 21:30:39 +0530

    url-backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/', // ensures correct paths for all assets
 });
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
